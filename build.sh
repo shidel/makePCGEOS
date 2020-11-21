82dcec801333e6af545b67bcc7ac1591e59d7076
Shidel <shidel@users.noreply.github.com>
2020-11-21 05:17:35 -0500

    initial commit

---
+++ build.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# cleanup to create fresh build
+function remove () {
+	while [[ "${1}" != "" ]] ; do
+		if [[ -e "${1}" ]] ; then
+			rm -rfv "${1}" || return $?
+		fi
+		shift
+	done
+	return 0
+}
+
+if [[ "${1}" == "clean" ]] ; then
+	remove watcom pcgeos ow-snapshot.tar.gz
+	exit 0
+fi
+
+# display some version information
+if [[ -e /etc/SUSE-brand ]] ; then
+	echo "$(head -n1 /etc/SUSE-brand)$(grep -i VERSION /etc/SUSE-brand | cut -d '=' -f 2)"	
+fi	
+uname -a
+sed --version | grep -i '^sed'
+perl --version | grep -i 'version'
+echo
+
+# fetch or update to latest repository version of PC-GEOS
+if [[ ! -d pcgeos ]] ; then
+	git clone https://github.com/bluewaysw/pcgeos.git
+else
+	cd pcgeos
+	git pull
+	cd ..
+fi
+
+# if not already downloaded, fetch latest version of WATCOM
+if [[ ! -d watcom ]] ; then
+	remove ow-snapshot.tar.gz
+	wget https://github.com/open-watcom/open-watcom-v2/releases/download/Current-build/ow-snapshot.tar.gz
+	mkdir watcom
+	cd watcom
+	tar -xvzf ../ow-snapshot.tar.gz
+	cd ..
+	remove ow-snapshot.tar.gz
+fi;
+
+SRCS=${PWD}
+# configure build environment
+export WATCOM=${PWD}/watcom
+export PATH=${PATH}:${WATCOM}/binl64:${WATCOM}/binnt:${PWD}/pcgeos/bin
+export ROOT_DIR=
+export LOCAL_DIR=
+
+# Build PC/GEOS SDK
+cd "${SRCS}/pcgeos/Tools/pmake/pmake"
+wmake install
+
+cd "${SRCS}/pcgeos/Installed/Tools"
+pmake
+
+cd "${SRCS}/pcgeos/Installed"
+pmake
+
+# Build target environment
+cd "${SRCS}/pcgeos/Tools/build/product/bbxensem/Scripts"
+perl -I. buildbbx.pl
+
+
+
